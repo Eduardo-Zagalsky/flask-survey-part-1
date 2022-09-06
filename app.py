c23f3016d5da8b1c2e2d60fe251da5237953b6d1
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2022-09-06 13:12:50 -0500

    added the logic for the root route

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask,request, redirect
+from flask import Flask,request, redirect, render_template
 from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "our-secret"
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+@app.route("/")
+def root_route():
+    title = satisfaction_survey.title
+    instructions = satisfaction_survey.instructions
+    return render_template("satisfaction.html", title = title, instructions = instructions)
