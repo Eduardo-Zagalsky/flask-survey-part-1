07568042c3357cd913ad2e4cdd7ef517e3f4b121
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2022-09-06 23:51:05 -0500

    Added flash extension and messages. added obj response for messages in thank you page. added the thank you page logic. added the if statements to prevent messing with the order of questions and to make sure it lands in the right place

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request, redirect, render_template
+from flask import Flask,request, redirect, render_template, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -7,6 +7,7 @@ app.config["SECRET_KEY"] = "our-secret"
 debug = DebugToolbarExtension(app)
 
 responses = []
+objresponses = {}
 
 @app.route("/")
 def root_route():
@@ -16,11 +17,21 @@ def root_route():
 
 @app.route("/questions/<int:q>")
 def question(q):
+    if len(responses) == len(satisfaction_survey.questions):
+        return redirect("/thankyou")
+    if len(responses) != q:
+        flash(f"{q} is not the next sequential question")
+        return redirect(f"/questions/{len(responses)}")
     question = satisfaction_survey.questions[q]
     return render_template("question.html", question = question, number = q)
 
 @app.route("/answer", methods=["POST"])
 def answers():
     answer = request.form.get("answer")
+    objresponses[satisfaction_survey.questions[len(responses)].question] = answer
     responses.append(answer)
     return redirect(f"/questions/{len(responses)}")
+
+@app.route("/thankyou")
+def completed():
+    return render_template("completed.html",responses=objresponses)
